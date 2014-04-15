086f66cf844b18def975d952c991775e23f3249f
Brady Brown <bobrown101@gmail.com>
2014-04-15 16:07:42 -0400

    Adjusted the padding on mobile

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require('./date')
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+var app = express(); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
