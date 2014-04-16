627c1a6329700bf99e2741eca7c16af379b9917d
Brady Brown <bobrown101@gmail.com>
2014-04-16 13:03:53 -0400

    Updated bad commit

--- public/javascripts/side-bar.js
+++ public/javascripts/side-bar.js
@@ -1,16 +1,10 @@
 $( document ).ready(function() {
   $('.close-menu').click(function(event) {
-<<<<<<< HEAD
     $('#nav').addClass('hide-nav');
   });
   $('#open-side-bar').click(function(event) {
-=======
+
     $('#content').removeClass('slide-content-over');
     $('#nav').addClass('hide-nav');
-  });
-  $('.hamburger').click(function(event) {
-    $('#content').addClass('slide-content-over');
->>>>>>> 8ec3d6db2cde71ef24d8b9ae7319e0d1ba3ce3e9
-    $('#nav').removeClass('hide-nav');
   });
 });
