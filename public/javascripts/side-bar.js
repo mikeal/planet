8ded0e3cff82ab96348199e0ba1c91e678292cfe
Brady Brown <bobrown101@gmail.com>
2014-04-16 16:48:10 -0400

    Added functionality to nav bar so whatever section they are in, the nav bar option is highlighted

--- public/javascripts/side-bar.js
+++ public/javascripts/side-bar.js
@@ -1,27 +1,31 @@
 $( document ).ready(function() {
   setUpMobileLayout();
-  $('.close-menu').click(function(event) { //this should eventually be REMOVED
-    $('#nav').addClass('hide-nav');
-  });
   $('#open-side-bar').click(function(event) {
     if ($('#nav').hasClass('hide-nav')) {
-      console.log('side-bar is currently hidden. showing now');
-      $('#nav').removeClass('hide-nav');
+      showNav();
     } else{
-      console.log('side bar shown. hiding now');
-      $('#nav').addClass('hide-nav');
+      hideNav();
     };
   });
 
   $('#nav-option-articles').click(function(event) {
     showArticles();
     hideSites();
+    hideNav();
   });
   $('#nav-option-sites').click(function(event) {
     showSites();
     hideArticles();
+    hideNav();
   });
 });
+
+function hideNav(){
+  $('#nav').addClass('hide-nav');
+}
+function showNav(){
+  $('#nav').removeClass('hide-nav');
+}
 
 function setUpMobileLayout(){
   showArticles();
@@ -32,11 +36,13 @@ function showArticles(){
   if ($('#posts').hasClass('hide-me')){
     $('#posts').removeClass('hide-me');
   }
+  showNavItemSelected($('#nav-option-articles'));
 }
 function showSites(){
   if ($('#links').hasClass('hide-me')){
     $('#links').removeClass('hide-me');
   }
+  showNavItemSelected($('#nav-option-sites'));
 
 }
 function hideArticles(){
@@ -44,4 +50,31 @@ function hideArticles(){
 }
 function hideSites(){
   $('#links').addClass('hide-me');
+}
+
+function isShown(el){
+  if ( ! el.hasClass('hide')){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+/*
+This function cycles through all of the nav options. 
+It deselects all of the ones that are not the element you passed in.
+Then it selects the one you did pass in
+ */
+function showNavItemSelected(el){
+  var navOptions = $('.nav-option');
+  
+  $.each(navOptions, function(){
+    
+    if(this.id != el.id){
+      $(this).removeClass('nav-selected');
+    }
+  });
+
+  el.addClass('nav-selected');
+  console.log($(el));
 }
