03c226335f262367b660936a3413b1b87ef16516
Brady Brown <bobrown101@gmail.com>
2014-04-26 17:34:31 -0400

    Added mongo db. Not sure if async yet

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,25 @@ var feedreader = require('../lib/feedreader.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  feedreader.run(function(rss){
-  	res.render('index', { data: rss });
+  var db = req.db;
+  var collection = db.get('usercollection');
+  
+  feedreader.run(collection, function(rss, docs) {
+    /*
+    FOR USE LATER - req.query will give you the url query. 
+    I plan on using the query for multiple pages - aka first 10 articles on 1 page, then next 10 on 2nd page...so on
+    Add a simple if statement to sort through them
+     */
+    
+    console.log(docs);
+    res.render(
+      'index', {data: rss, test: docs}
+    );
+      
   });
 });
+
+
+
 
 module.exports = router;
