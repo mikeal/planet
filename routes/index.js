b428df5ceb8c67a5ccc98a048b6bc9a7e8aa664b
Brady Brown <bobrown101@gmail.com>
2014-04-29 17:51:40 -0400

    Further improvements to addition of mongo. Still not production ready

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,17 @@ var feedreader = require('../lib/feedreader.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  var db = req.db;
-  var collection = db.get('usercollection');
+
   
-  feedreader.run(collection, function(rss, docs) {
+  feedreader.run(function(posts) {
     /*
     FOR USE LATER - req.query will give you the url query. 
     I plan on using the query for multiple pages - aka first 10 articles on 1 page, then next 10 on 2nd page...so on
     Add a simple if statement to sort through them
      */
-    
-    console.log(docs);
+  
     res.render(
-      'index', {data: rss, test: docs}
+      'test', {data: posts}
     );
       
   });
