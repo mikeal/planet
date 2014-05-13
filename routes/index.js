2066df38e1beb0c9191edebae6ccdf62e01afa21
Brady Brown <bobrown101@gmail.com>
2014-05-13 16:38:26 -0400

    Fixed somemore

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,8 @@ router.get('/', function(req, res) {
     I plan on using the query for multiple pages - aka first 10 articles on 1 page, then next 10 on 2nd page...so on
     Add a simple if statement to sort through them
      */
-<<<<<<< HEAD
-    res.render(
-      'index', {data: posts}
-=======
-  
     res.render(
       'test', {data: posts}
->>>>>>> 2cca3be40c0276b954f8170efdc996e6ec04471e
     );
       
   });
