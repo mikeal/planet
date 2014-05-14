181fb7041553ad43309428a91c889ff74a499768
Brady Brown <bobrown101@gmail.com>
2014-05-14 16:35:22 -0400

    Added back the sites bar

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,15 @@ var feedreader = require('../lib/feedreader.js');
 router.get('/', function(req, res) {
 
   
-  feedreader.run(function(posts) {
+  feedreader.run(function(posts, sitesArray) {
     /*
     FOR USE LATER - req.query will give you the url query. 
     I plan on using the query for multiple pages - aka first 10 articles on 1 page, then next 10 on 2nd page...so on
     Add a simple if statement to sort through them
      */
+    console.log('sites from index.js - ' + sitesArray);
     res.render(
-      'test', {data: posts}
+      'index', {data: posts, sitesList: sitesArray}
     );
       
   });
