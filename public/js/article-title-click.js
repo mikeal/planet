e9c5ed5fdef2cf2bd0c1dbed1f6c13bf6dad3743
Brady Brown <bobrown101@gmail.com>
2014-05-22 16:52:27 -0400

    now automagically closes other open articles, if you open another one

--- public/js/article-title-click.js
+++ public/js/article-title-click.js
@@ -1,4 +1,5 @@
 $('.post-title').click(function() {
+    $('.in-depth').addClass('hide');
     var content = $(this).closest('.post').find('.in-depth');
     if($(content).hasClass('hide')){
         $(content).removeClass('hide');
