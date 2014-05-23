e339c2d344a69151877ecedfdd5266a088c262dc
Brady Brown <bobrown101@gmail.com>
2014-05-23 11:44:25 -0400

    Added colors to console output - makes it more readable now

--- public/js/article-title-click.js
+++ public/js/article-title-click.js
@@ -1,5 +1,9 @@
 $('.post-title').click(function() {
-    $('.in-depth').addClass('hide');
+    for (var i = 0; i < $('.post-title').length; i++) {
+        if( $(this) != $('.post-title')[i]){
+            $('.post-title')[i].addClass('hide');
+        }
+    };
     var content = $(this).closest('.post').find('.in-depth');
     if($(content).hasClass('hide')){
         $(content).removeClass('hide');
