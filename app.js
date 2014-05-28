6d7308239b655b54751e3ebd237794bab66c8254
Brady Brown <bobrown101@gmail.com>
2014-05-28 16:43:33 -0400

    Updated the views and made a better error page

--- app.js
+++ app.js
@@ -47,10 +47,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.render('error');
 });
 
 
