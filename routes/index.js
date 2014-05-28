ede8e69ec3f981d04800d5ef18416ef9dff94898
Brady Brown <bobrown101@gmail.com>
2014-05-27 20:58:50 -0400

    Added search functionality

--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,31 @@ var feedreader = require('../lib/feedreader.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  console.log(req.query.searchString);
+
   if(req.query.id){
-      console.log(('Here is the id of the document the user is trying to access: ' + req.query.id));
-      feedreader.getArticleById(req.query.id, function(post){
-        var article = [];
-        article[0] = post;
-        res.render(
-          'singleArticle', {data: article}
-        );
-      });
+    console.log(('Here is the id of the document the user is trying to access: ' + req.query.id));
+    feedreader.getArticleById(req.query.id, function(post){
+      var article = [];
+      article[0] = post;
+      res.render(
+        'singleArticle', {data: article}
+      );
+    });
   }
   else if(req.query.searchString){
-
+    console.log(('Here is the users search: ' + req.query.searchString));
+    feedreader.getArticlesBySearchString(req.query.searchString, function(results){
+      console.log(results.hits.hits.length);
+      console.log(results.hits.hits[0]._source.title);
+      res.render(
+        'search', {data: results}
+      );
+    });
   }
   else {
-    feedreader.run(function(posts, sitesArray) {    
+    feedreader.run(function(posts) {    
       res.render(
-        'index', {data: posts, sitesList: sitesArray}
+        'index', {data: posts}
       );
         
     });
