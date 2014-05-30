35e705652a531dc2c324cebbdd20abee0dc03c61
Brady Brown <bobrown101@gmail.com>
2014-05-29 20:57:12 -0400

    Navbar now hides when user scrolls down, and shows when scrolling up - with really cool animation btw

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+
+$("#nav").headroom({
+  "tolerance": 5,
+  "offset": 205,
+  "classes": {
+    "initial": "animated",
+    "pinned": "slideDown",
+    "unpinned": "slideUp",
+    "top": "headroom--top",
+    "notTop": "headroom--not-top"
+  }
+});
